cfc900a96f4b30b7ea3a6ace6fe117e3dfbeed23
Higor <higorpiresoliveira@gmail.com>
2022-02-12 16:39:33 -0300

    create route to find one property

--- packages/properties/src/controllers/PropertiesController.ts
+++ packages/properties/src/controllers/PropertiesController.ts
@@ -2,13 +2,16 @@ import { Request, Response } from "express";
 import { validationResult } from "express-validator";
 
 import { AlreadyExistsException } from "../exceptions/AlreadyExistsException";
+import { NotFound } from "../exceptions/NotFound";
 import { CreatePropertyService } from "../services/CreatePropertyService";
+import { FindPropertyService } from "../services/FindPropertyService";
 import { ListPropertiesService } from "../services/ListPropertiesService";
 
 class PropertiesController {
     constructor(
         private createPropertyService: CreatePropertyService,
-        private listPropertiesService: ListPropertiesService
+        private listPropertiesService: ListPropertiesService,
+        private findPropertyService: FindPropertyService
     ) {
         //
     }
@@ -41,6 +44,21 @@ class PropertiesController {
         const properties = await this.listPropertiesService.execute();
 
         return res.json(properties).send();
+    }
+
+    async find(req: Request, res: Response) {
+        const { id: propertyId } = req.params;
+
+        try {
+            const property = await this.findPropertyService.execute(propertyId);
+
+            return res.json(property).send();
+        } catch (err) {
+            if (err instanceof NotFound) {
+                return res.status(404).send({ error: err.message });
+            }
+            return res.status(500).send({ error: "Unexpected error" });
+        }
     }
 }
 
--- packages/properties/src/routes/properties.routes.ts
+++ packages/properties/src/routes/properties.routes.ts
@@ -2,18 +2,22 @@ import { Router } from "express";
 
 import { PropertiesController } from "../controllers/PropertiesController";
 import { CreatePropertyServiceImpl } from "../services/CreatePropertyServiceImpl";
+import { FindPropertyServiceImpl } from "../services/FindPropertyServiceImpl";
 import { ListPropertiesServiceImpl } from "../services/ListPropertiesServiceImpl";
 
 const PropertiesRoutes = Router();
 
 const createPropertyService = new CreatePropertyServiceImpl();
 const listPropertiesService = new ListPropertiesServiceImpl();
+const findPropertyService = new FindPropertyServiceImpl();
 const controller = new PropertiesController(
     createPropertyService,
-    listPropertiesService
+    listPropertiesService,
+    findPropertyService
 );
 
 PropertiesRoutes.get("/", (req, res) => controller.list(req, res));
+PropertiesRoutes.get("/:id", (req, res) => controller.find(req, res));
 PropertiesRoutes.post("/", (req, res) => controller.create(req, res));
 
 export { PropertiesRoutes };
