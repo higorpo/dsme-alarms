46f0982c5bf359049c3cc101bbb3c4e40b3b73c5
Higor <higorpiresoliveira@gmail.com>
2022-02-12 16:14:58 -0300

    create route to list all properties

--- packages/properties/src/controllers/PropertiesController.ts
+++ packages/properties/src/controllers/PropertiesController.ts
@@ -3,9 +3,13 @@ import { validationResult } from "express-validator";
 
 import { AlreadyExistsException } from "../exceptions/AlreadyExistsException";
 import { CreatePropertyService } from "../services/CreatePropertyService";
+import { ListPropertiesService } from "../services/ListPropertiesService";
 
 class PropertiesController {
-    constructor(private createPropertyService: CreatePropertyService) {
+    constructor(
+        private createPropertyService: CreatePropertyService,
+        private listPropertiesService: ListPropertiesService
+    ) {
         //
     }
 
@@ -31,6 +35,12 @@ class PropertiesController {
             }
             return res.status(500).send({ error: "Unexpected error" });
         }
+    }
+
+    async list(req: Request, res: Response) {
+        const properties = await this.listPropertiesService.execute();
+
+        return res.json(properties).send();
     }
 }
 
--- packages/properties/src/routes/properties.routes.ts
+++ packages/properties/src/routes/properties.routes.ts
@@ -2,12 +2,18 @@ import { Router } from "express";
 
 import { PropertiesController } from "../controllers/PropertiesController";
 import { CreatePropertyServiceImpl } from "../services/CreatePropertyServiceImpl";
+import { ListPropertiesServiceImpl } from "../services/ListPropertiesServiceImpl";
 
 const PropertiesRoutes = Router();
 
 const createPropertyService = new CreatePropertyServiceImpl();
-const controller = new PropertiesController(createPropertyService);
+const listPropertiesService = new ListPropertiesServiceImpl();
+const controller = new PropertiesController(
+    createPropertyService,
+    listPropertiesService
+);
 
+PropertiesRoutes.get("/", (req, res) => controller.list(req, res));
 PropertiesRoutes.post("/", (req, res) => controller.create(req, res));
 
 export { PropertiesRoutes };
